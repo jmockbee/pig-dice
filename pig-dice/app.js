3437ab94f869f3af3a8e096aec4f7269666d9b24
jmockbee <mockbee1000@gmail.com>
2018-08-15 22:18:13 -0400

    fixed the problem with the dice

--- pig-dice/app.js
+++ pig-dice/app.js
@@ -8,20 +8,21 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer;
-/*
+var scores, roundScore, activePlayer,dice;
+
 scores = [ 0,0];
 roundScore = 0;
 activePlayer = 1;
 
-// testing in console
-//dice = Math.floor(Math.random() * 6) + 1;
-//console.log(dice);
 
-//document.querySelector('#current-'+activePlayer). textContent = dice;
+
+//document.querySelector('#current-'+ activePlayer).textContent = dice;
 //document.querySelector('#current-'+activePlayer).innerHTML = '<em>'+ dice + '</em>';
 
-document.querySelector('.dice').style.display = 'none';
+//var x =document.querySelector('#score-0'). textContent;
+//console.log (x);
+
+document.querySelector('.dice').style.display = 'block';
 
 
 
@@ -31,16 +32,16 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
 dice = Math.floor(Math.random() * 6) + 1;
 
 // 2. result display
-var diceDOM  = document.querySelector('.dice');
-diceDom.style.display = 'block';
-diceDOM.src = 'dice-' + dice +'.png';
+var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
 
 
 
 });
 
 
- */
+ 
 
 
 
