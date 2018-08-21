a9d1e011321fe2174cb6ea49c77b70fa48a6359d
jmockbee <mockbee1000@gmail.com>
2018-08-20 20:50:31 -0400

    changed the background so matched the player

--- pig-dice/app.js
+++ pig-dice/app.js
@@ -13,7 +13,7 @@ var scores, roundScore, activePlayer,dice;
 
 scores = [ 0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -48,8 +48,8 @@ if (dice !== 1) {
         document.getElementById ('current-0').textContent = '0';
         document.getElementById ('current-1').textContent = '0';
 
-        document.querySelector('.player-0-panel').classList.toggle('active');
         document.querySelector('.player-1-panel').classList.toggle('active');
+        document.querySelector('.player-0-panel').classList.toggle('active');
        // document.querySelector('.player-0-panel').classList.remove('active');
         //document.querySelector('.player-1-panel').classList.add('active');
 
